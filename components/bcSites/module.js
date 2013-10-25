f92cc12d2dc0fdcffabd52d5bf4897de954f013b
Vitalii Savchuk <esvit666@gmail.com>
2013-10-25 15:52:41 +0300

    Merge branch 'master' of https://github.com/esvit/bazalt
    Conflicts:
    	bower.json
    	modules/bzUploader/directives/bzUploader.js

--- components/bcSites/module.js
+++ components/bcSites/module.js
@@ -4,11 +4,4 @@ define('components/bcSites/module', [
     'components/bcSites/factories/Site'
 ], function (app) {
     'use strict';
-
-    app.run(['$rootScope', 'bcSites.Factories.Site', function($rootScope, SiteResource) {
-
-        $rootScope.$site = SiteResource.get({'id': 'current'}, function(site) {
-             //site;
-        });
-    }]);
 });
