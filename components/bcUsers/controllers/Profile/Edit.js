2fcbce1dfc2c1d3458f3f5df8f80885fd13bb891
oll-e@equalteam.net <oll.rudenko@gmail.com>
2013-11-06 17:11:04 +0200

    edit container

--- components/bcUsers/controllers/Profile/Edit.js
+++ components/bcUsers/controllers/Profile/Edit.js
@@ -7,6 +7,7 @@ define('components/bcUsers/controllers/Profile/Edit', [
         ['$scope', 'bcUsers.Factories.User', '$rootScope', '$location', '$user',
             function ($scope, UserResource, $rootScope, $location, $user) {
 
+                $scope.gift = {"item_id":"32","gift_id":"1","user_id":"23","to_id":"22","message":"","status":0,"created_at":"1383640261000","updated_at":"1383640261000","to":{"id":"22","login":"oll.rudenko@gmail.com","firstname":"Виктория","secondname":"Кирова","patronymic":null,"avatar":"/uploads/5b/49/5b4910c2cea3912cb8e657c73bfc8e1a.jpg","email":"oll.rudenko@gmail.com","gender":"female","birth_date":"18.06.1987","is_active":true,"is_deleted":false,"last_activity":"2013-11-05 15:10:06","created_at":"2013-11-03 16:17:24","fullname":"Кирова Виктория","permissions":[]},"from":{"id":"23","login":"my.impik@gmail.com","firstname":"Дмитрий","secondname":"Кунин","patronymic":null,"avatar":"/uploads/22/b6/22b61b12b9cd20d035716d678d76bebc.jpg","email":"my.impik@gmail.com","gender":"male","birth_date":"11.06.1980","is_active":true,"is_deleted":false,"last_activity":"2013-11-06 13:49:18","created_at":"2013-11-03 17:08:39","fullname":"Кунин Дмитрий","permissions":[]}};
                 $scope.user = UserResource.get({ 'id': $user.data.id }, function(user) {
                     $scope.loading = false;
                     if (!user.images) {
